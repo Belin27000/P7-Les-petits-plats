5a1cd1a4fe6179146d924ce49d47fae0d6cf160e
Belin27000 <y.lecerf@protonmail.com>
2023-07-04 10:39:21 +0200

    Algo 2 ForEach

--- scripts/utils/searchRecipe.js
+++ scripts/utils/searchRecipe.js
@@ -15,14 +15,14 @@ function searchBar(recipes) {
     });
 }
 
-//---Algo1 - fonction de tri des recettes en fonction des filtres déjà cliqué---//
+
+//---Algo2 - fonction de tri des recettes en fonction des filtres déjà cliqué---//
 function tagFilter(recipes, filterTagOption) {
-    console.log(recipes.length);
     let newRecipes = [];
-    // console.(allTags);
-    for (let recipe of recipes) {
+
+    recipes.forEach(recipe => {
         let keep = true;
-        for (const tag of allTags) {
+        allTags.forEach(tag => {
             switch (tag.type) {
                 case 'appliance':
                     if (!recipe.appliance.toLowerCase().match(tag.value.toLowerCase())) {
@@ -32,14 +32,15 @@ function tagFilter(recipes, filterTagOption) {
 
                 case 'ingredients':
                     const tagIngredient = [];
-                    for (const ingre of recipe.ingredients) {
+                    let recipeIngredients = recipes.ingredients;
+                    recipeIngredients.forEach(ingre => {
                         let ingredientRecipe = ingre.ingredient.toLowerCase();
                         let tagValue = tag.value.toLowerCase();
                         let result = ingredientRecipe.localeCompare(tagValue);//compare les 2 chaine de caractères afin de renvoyer uniquement le resultat identiques renvoi 0 si identique
                         if (result == 0) {
                             tagIngredient.push(ingre);
                         }
-                    }
+                    });
                     if (tagIngredient.length == 0) {
                         keep = false;
                     }
@@ -47,35 +48,38 @@ function tagFilter(recipes, filterTagOption) {
 
                 case 'ustensils':
                     const tagUstensil = [];
-                    for (const ustensil of recipe.ustensils) {
+                    let recipeUstensil = recipe.ustensils;
+                    recipeUstensil.forEach(ustensil => {
                         let ustensilRecipe = ustensil.toLowerCase();
                         let tagValue = tag.value.toLowerCase();
                         let result = ustensilRecipe.localeCompare(tagValue);//compare les 2 chaine de caractères afin de renvoyer uniquement le resultat identiques renvoi 0 si identique
-
                         if (result == 0) {
                             tagUstensil.push(ustensil);
                         }
-                    }
+                    });
+
                     if (tagUstensil.length == 0) {
                         keep = false;
                     }
                     break;
             }
-        }
+        });
         if (filterTagOption === 'searchBarEmpty' && keep) {
             newRecipes.push(recipe);
 
         } else if (filterTagOption === 'searchBarFilled' && keep) {
             let ingredientString = ''; // On initialise une variable d'une chaîne de caractère vide
-            for (const item of recipe.ingredients) {
+            let recipeArray = recipe.ingredients; // on charge le tableau des ingredients pour chaque recettes
+            recipeArray.forEach(item => {
                 // On parcourt les ingrédients d'une recette
                 ingredientString += item.ingredient + ' '; // On ajoute à la variable de chaîne de caractère, les ingrédients de chaque recette parcourue
-            }
+            });
+
             // Vérification du champs saisie pour ne retourner que les recettes qui contiennent la valeur saisie par l'utilisateur
             if (recipe.name.toLowerCase().match(searchRecipes.value.toLowerCase()) || recipe.description.toLowerCase().match(searchRecipes.value.toLowerCase()) || ingredientString.toLowerCase().match(searchRecipes.value.toLowerCase())) {
                 newRecipes.push(recipe);
             }
         }
-    }
-    return newRecipes;
+    });
+    return newRecipes
 }
